aa77768715d0e791122c95240cb269df1088d631
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-12 04:33:10 +0330

    Slider done

---
+++ app/components/Slider/Slider.jsx
@@ -0,0 +1,75 @@
+"use client";
+import styles from "./slider.module.css";
+
+import React from "react";
+import Image from "next/image";
+import image1 from "../../../public/coffee1.jpg";
+import image2 from "../../../public/coffee2.jpg";
+import image3 from "../../../public/coffee3.jpg";
+import image4 from "../../../public/coffee4.jpg";
+import image5 from "../../../public/coffee5.jpg";
+import image6 from "../../../public/coffee6.jpg";
+import image7 from "../../../public/coffee7.jpg";
+import image8 from "../../../public/coffee8.jpg";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+export default function Slider() {
+  return (
+    <div className={styles.container}>
+      <h2>Menu Gallery</h2>
+      <p>Take a look at the magic we do in our coffee house ...</p>
+      <Swiper
+        spaceBetween={30}
+        loop={true}
+        slidesPerView={3}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className={styles.slider}
+      >
+        <SwiperSlide>
+          <Image src={image1} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={image2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={image3} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={image4} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={image5} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={image6} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={image7} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={image8} />
+        </SwiperSlide>
+      </Swiper>
+      <button className={styles.menuBtn}>Read The Menu</button>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Top from "./components/Top/Top";
+import Slider from "./components/Slider/Slider";
 
 export default function Home() {
   return (
     <main>
       <Top />
+      <Slider />
     </main>
   );
 }

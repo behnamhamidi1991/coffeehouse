461dc2d5f3d327c954fc30012b2bfc6c4e655a39
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-12 04:53:36 +0330

    Coffee house

---
+++ app/components/CoffeeHouse/CoffeeHouse.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./coffeehouse.module.css";
+import Image from "next/image";
+import image1 from "../../../public/coffeeshop1.jpg";
+import image2 from "../../../public/coffeeshop2.jpg";
+import image3 from "../../../public/coffeeshop3.jpg";
+
+const CoffeeHouse = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Our Coffee House</h2>
+      <p>You can come and visit our amazing environment ... </p>
+      <div className={styles.imageContainer}>
+        <div className={styles.boxContainer}>
+          <Image src={image1} alt="coffee-shop" className={styles.imageItem} />
+          <p className={styles.imageDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+            ipsum aspernatur at, fugiat porro cumque laborum maiores ut illo
+            illum enim, temporibus eveniet recusandae nesciunt, labore unde
+            blanditiis doloremque inventore.
+          </p>
+        </div>
+
+        <div className={styles.boxContainer}>
+          <Image src={image2} alt="coffee-shop" className={styles.imageItem} />
+          <p className={styles.imageDesc2}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+            ipsum aspernatur at, fugiat porro cumque laborum maiores ut illo
+            illum enim, temporibus eveniet recusandae nesciunt, labore unde
+            blanditiis doloremque inventore.
+          </p>
+        </div>
+
+        <div className={styles.boxContainer}>
+          <Image src={image3} alt="coffee-shop" className={styles.imageItem} />
+          <p className={styles.imageDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+            ipsum aspernatur at, fugiat porro cumque laborum maiores ut illo
+            illum enim, temporibus eveniet recusandae nesciunt, labore unde
+            blanditiis doloremque inventore.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeHouse;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Top from "./components/Top/Top";
 import Slider from "./components/Slider/Slider";
+import CoffeeHouse from "./components/CoffeeHouse/CoffeeHouse";
 
 export default function Home() {
   return (
     <main>
       <Top />
       <Slider />
+      <CoffeeHouse />
     </main>
   );
 }

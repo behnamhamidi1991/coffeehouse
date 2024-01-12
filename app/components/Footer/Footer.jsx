d7c6456c627334114ea4de3a1992c473481b6c55
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-12 05:28:00 +0330

    Final commit

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <footer>Footer</footer>;
 };
 
 export default Footer;
